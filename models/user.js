e87f5903de154e81e7b6ca4fb1b75272466db1f0
Alainr701 <a701e3127@gmail.com>
2021-12-31 16:53:10 -0400

    User Token Create

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const db = require('../config/config');
+const crypto = require('crypto');
+
 
 const User = {};
 User.getAll = () => {
@@ -12,9 +14,53 @@ User.getAll = () => {
     return db.manyOrNone(sql);
 }
 
+
+User.findByEmail = (email) => {
+    const sql = `
+    SELECT
+        id,
+        email,
+        name,
+        lastname,
+        image,
+        phone,
+        password,
+        session_token
+    FROM 
+        users
+    WHERE
+        email = $1
+    `
+    return db.oneOrNone(sql, email);
+}
+
+
+
+
+User.findById = (id, callback) => {
+
+    const sql = `
+    SELECT
+        id,
+        email,
+        name,
+        lastname,
+        image,
+        phone,
+        password,
+        session_token
+    FROM
+        users
+    WHERE
+        id = $1`;
+    
+    return db.oneOrNone(sql, id).then(user => { callback(null, user); })
+
+}
+
 User.create = (user) => {
-    // const myPasswordHashed = crypto.createHash('md5').update(user.password).digest('hex');
-    // user.password = myPasswordHashed;
+    const myPasswordHashed = crypto.createHash('md5').update(user.password).digest('hex');
+    user.password = myPasswordHashed;
     const sql = `
     INSERT INTO
         users(
@@ -40,6 +86,14 @@ User.create = (user) => {
         new Date(),
         new Date()
     ]);
+}
+
+User.isPasswordMatched = (userPassword, hash) => {
+    const myPasswordHashed = crypto.createHash('md5').update(userPassword).digest('hex');
+    if (myPasswordHashed === hash) {
+        return true;
+    }
+    return false;
 }
 
 module.exports = User;
